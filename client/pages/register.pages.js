9b04580af1c0061443409fc87e3ce1cae065d1ed
Conan Lee <liyijia@shinetechchina.com>
2017-01-24 17:52:34 +0800

    use icons

--- client/pages/register.pages.js
+++ client/pages/register.pages.js
@@ -1,10 +1,20 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import WhiteTextField from '../components/WhiteTextField/WhiteTextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import ActionAndroid from 'material-ui/svg-icons/action/android';
+import IconButton from 'material-ui/IconButton';
 class Register extends Component {
     render() {
         return (
-            <div>
-                this is register
+            <div className='column-center center-flex'>
+                <div>
+                    <WhiteTextField floatLabel='Mobile'></WhiteTextField>
+                    <IconButton iconClassName='material-icons'>
+                        home
+                    </IconButton>
+                </div>
+                <RaisedButton className='account-btn' label='Get Started' primary={true} />
+                <div></div>
             </div>
         );
     }
