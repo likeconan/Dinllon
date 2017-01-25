e30f9df65b2eff97bc0364c848f36f59e39c57b5
likeconan <liyijia@126.com>
2017-01-25 08:28:40 +0800

    change register

--- client/pages/register.pages.js
+++ client/pages/register.pages.js
@@ -3,18 +3,19 @@ import WhiteTextField from '../components/WhiteTextField/WhiteTextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import ActionAndroid from 'material-ui/svg-icons/action/android';
 import IconButton from 'material-ui/IconButton';
+import { grey50 } from 'material-ui/styles/colors';
 class Register extends Component {
     render() {
         return (
-            <div className='column-center center-flex'>
-                <div>
+            <div className='center-flex'>
+                <div className='center-flex column-center'>
                     <WhiteTextField floatLabel='Mobile'></WhiteTextField>
-                    <IconButton iconClassName='material-icons'>
-                        home
-                    </IconButton>
+                    <WhiteTextField floatLabel='SMS Code'></WhiteTextField>
+                    <RaisedButton className='account-btn' label='Get Started' primary={true} />
                 </div>
-                <RaisedButton className='account-btn' label='Get Started' primary={true} />
-                <div></div>
+                <IconButton iconClassName='material-icons' style={{alignSelf:'flex-start',marginTop:'20px'}} iconStyle={{ color: grey50 }}>
+                    email
+                </IconButton>
             </div>
         );
     }
