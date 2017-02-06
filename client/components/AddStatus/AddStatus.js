21714a6b4d0515e3890425637b004e7048a863b8
Conan Lee <liyijia@shinetechchina.com>
2017-02-06 17:57:26 +0800

    Try to use dropzone

--- client/components/AddStatus/AddStatus.js
+++ client/components/AddStatus/AddStatus.js
@@ -3,9 +3,25 @@ import TextareaAutosize from 'react-autosize-textarea';
 import { UserModel } from '../../models';
 import IconButton from 'material-ui/IconButton';
 import { blueA400 } from 'material-ui/styles/colors';
+import Dropzone from 'react-dropzone';
+
+
 
 require('./add-status.less');
 class AddStatus extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            files: []
+        };
+    }
+
+    onDrop = (files) => {
+        this.setState({
+            files: this.state.files.concat(files)
+        })
+    }
+
     render() {
         var user = new UserModel();
         return (
@@ -13,11 +29,18 @@ class AddStatus extends Component {
                 <img src={user.headPic} className='head-pic' />
                 <div className='add-status-con'>
                     <TextareaAutosize className='add-text grey-text text-darken-2 sans-font' style={{ minHeight: 24 }} />
-                    <IconButton iconClassName="material-icons" className='add-photo' iconStyle={{ color: blueA400 }}>
-                        add_a_photo
-                    </IconButton>
+                    <Dropzone onDrop={this.onDrop} multiple={true}>
+                        <IconButton iconClassName="material-icons" className='add-photo' iconStyle={{ color: blueA400 }}>
+                            add_a_photo
+                        </IconButton>
+                    </Dropzone>
                 </div>
-
+                {this.state.files ?
+                    (
+                        <div>{this.state.files.map((file, key) => <img style={{ width: '50px' }} src={file.preview} key={key} />)}</div>
+                    )
+                    : null
+                }
             </add-status>
         );
     }
