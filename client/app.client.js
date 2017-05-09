aa9d08684be39f562cf6deabb9a38a90d47425f6
Conan Lee <liyijia@shinetechchina.com>
2017-05-09 18:02:01 +0800

    Make use of socket.io

--- client/app.client.js
+++ client/app.client.js
@@ -1,4 +1,4 @@
-import {Router, Route, browserHistory} from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 import React from 'react'
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -6,7 +6,8 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import pages from './pages';
 import store from './store';
-import {Provider} from 'react-redux';
+
+import { Provider } from 'react-redux';
 
 require("./styles/layout.less");
 injectTapEventPlugin();
@@ -22,19 +23,26 @@ const customMui = getMuiTheme({
     }
 });
 
+var socket = require('socket.io-client')('http://localhost:9000');
+socket.on('connect', function () { console.log('Socket IO connect') });
+socket.on('testevent', function (data) { console.log(data) });
+socket.on('disconnect', function () { });
+
+
+
 ReactDOM.render(
     <Provider store={store}>
-    <MuiThemeProvider muiTheme={customMui}>
-        <Router history={browserHistory}>
-            <Route component={pages.Layout}>
-                <Route path="/" component={pages.Home}/>
-                <Route path="/life" component={pages.SocialLife}/>
-                <Route component={pages.AccountLayout}>
-                    <Route path='/login' component={pages.Login}/>
-                    <Route path='/register' component={pages.Register}/>
+        <MuiThemeProvider muiTheme={customMui}>
+            <Router history={browserHistory}>
+                <Route component={pages.Layout}>
+                    <Route path="/" component={pages.Home} />
+                    <Route path="/life" component={pages.SocialLife} />
+                    <Route component={pages.AccountLayout}>
+                        <Route path='/login' component={pages.Login} />
+                        <Route path='/register' component={pages.Register} />
+                    </Route>
+                    <Route path="/profile" component={pages.Profile} />
                 </Route>
-                <Route path="/profile" component={pages.Profile}/>
-            </Route>
-        </Router>
-    </MuiThemeProvider>
-</Provider>, document.getElementById('root'));
+            </Router>
+        </MuiThemeProvider>
+    </Provider>, document.getElementById('root'));
